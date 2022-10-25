a5ff2e0353150e7a307db5426b2562070f357d31
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-25 18:04:11 +0100

    Add to columns, iscompleted, and is_cancelled to shopping list tabe

--- db/migrate/20221020224819_create_shopping_lists.rb
+++ db/migrate/20221020224819_create_shopping_lists.rb
@@ -2,6 +2,8 @@ class CreateShoppingLists < ActiveRecord::Migration[7.0]
   def change
     create_table :shopping_lists do |t|
       t.string :name
+      t.boolean :is_completed, null: false, default: false     
+      t.boolean :is_cancelled, null: false, default: false    
 
       t.timestamps
     end
